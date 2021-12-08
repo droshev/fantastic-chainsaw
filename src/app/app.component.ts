e955a12cb997c831efa2bd3075dc60f330720e42
droshev <mladen.droshev@sap.com>
2021-12-08 00:16:13 -0500

    feat: create new app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { WizardGeneratorItem } from '@fundamental-ngx/platform/wizard-generator';
+
+import { WizardDataService } from './wizard-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ngx-wizard';
+export class AppComponent implements OnInit {
+  stepItems: WizardGeneratorItem[] = [];
+
+  constructor(private wizardService: WizardDataService) {}
+  
+  ngOnInit(): void {
+    this.getWizardData();
+  }
+
+  getWizardData(){
+   this.stepItems = this.wizardService.getUserData();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { PlatformWizardGeneratorModule } from '@fundamental-ngx/platform/wizard-generator';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +16,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    PlatformWizardGeneratorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
